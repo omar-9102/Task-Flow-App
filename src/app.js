74666495013e7283c2543c2bc4ccd1f00a60a786
Omar Mohamed Abdelghafar <90798470+omar-9102@users.noreply.github.com>
2025-12-19 16:48:15 +0200

    Uncomment global error handler and remove unused code
    Uncomment global error handler and clean up commented code.

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const httpStatusText = require('./utils/httpStatusText')
 const path = require('path')
-// app.use('./uploads', express.static(path.join(__dirname,'/uploads')))
-// const multer = require('multer');
-// const upload = multer();
+
 
 const taskRouter = require('./routes/taskRoutes')
 const userRouter = require('./routes/userRoutes')
@@ -23,7 +21,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/tasks', taskRouter)
 app.use('/api/users', userRouter)
 
-//! Global Error Handler
+// Global Error Handler
 app.use((error, req, res, next) =>{
     res.status(error.statusCode || 500)
     .json({status: error.statusCode || httpStatusText.ERROR, message: error.message, code: error.statusCode || 500});
